46da46ee45f7f433d2cee286b45e998ec1ece8a2
JoonasRiivari <jonttis@gmail.com>
2023-02-20 09:23:00 +0200

    Hakemusten tarkistus lisätty

--- lisaahakemus.php
+++ lisaahakemus.php
@@ -1,7 +1,14 @@
 <?php
 
 $json = isset($_POST["hakemus"]) ? $_POST["hakemus"] : "";
+
+
 $hakemus = json_decode($json, false);
+
+if (!($kala = tarkistaJson($json))) {
+    print "Täytä kaikki kentät";
+    exit;
+}
 
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 
@@ -16,7 +23,9 @@ $sql = "insert into hakemus (etunimi, sukunimi, puhelinnumero, osoite, postinume
     "values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 $stmt = mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param( $stmt,'sssssssss', $hakemus->etunimi, $hakemus->sukunimi, $hakemus->puhelinnumero, $hakemus->osoite, $hakemus->postinumero, $hakemus->postitoimipaikka, $hakemus->syntymaaika, $hakemus->sahkoposti, $hakemus->lisatietoa,
+mysqli_stmt_bind_param(
+    $stmt,
+    'sssssssss', $hakemus->etunimi, $hakemus->sukunimi, $hakemus->puhelinnumero, $hakemus->osoite, $hakemus->postinumero, $hakemus->postitoimipaikka, $hakemus->syntymaaika, $hakemus->sahkoposti, $hakemus->lisatietoa,
 );
 mysqli_stmt_execute($stmt);
 mysqli_close($yhteys);
@@ -24,3 +33,16 @@ print "<h2>Kiitos hakemuksestasi!</h2>";
 print "Olemme yhteydessä teihin, mikäli valintamme kohdistuu teihin!";
 ?>
 
+
+<?php
+function tarkistaJson($json)
+{
+    $hakemus = json_decode($json, false);
+    if (empty($hakemus->etunimi) || empty($hakemus->sukunimi) || empty($hakemus->sukunimi) 
+    || empty($hakemus->puhelinnumero) || empty($hakemus->osoite) || empty($hakemus->postinumero) 
+    || empty($hakemus->postitoimipaikka) || empty($hakemus->syntymaaika) || empty($hakemus->sahkoposti)) {
+        return false;
+    }
+    return $hakemus;
+}
+?>
